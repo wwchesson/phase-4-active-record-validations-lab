8aa49471b711f4b48adb800bf18bd0620fee9d7a
Will <willchesson@gmail.com>
2022-07-06 12:09:24 -0400

    commit tree 542109e967d28cad2a6cfcc1dec7dd93ec30403f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+
+    validates :content, length: {minimum: 250}
+
+    validates :summary, length: {maximum: 250}
+
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+    validate :clickbait
+
+#     POSSIBLE_TITLES = [
+#     /Won't Believe/i,
+#     /Secret/i,
+#     /Top \d/i,
+#     /Guess/i
+# ]
+    
+    POSSIBLE_TITLES = ["Won't Believe", "Secret", "Top 10", "Guess"]
+
+    def clickbait
+        # unless POSSIBLE_TITLES.any? {|element| element.match(title)}
+        unless POSSIBLE_TITLES.any? {|element| title && title.include?(element)}
+            errors.add(:title, "Not sufficiently clickbait-y")
+        end
+    end
 end
